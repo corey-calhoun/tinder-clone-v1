ca2f0aed93326bfd0e1a97a7a247859e336c2da7
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-12-19 22:46:10 -0600

    initial project setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBR5cHWgr64Cgc1aaFqn4JwfM0DWDuClBo",
+    authDomain: "tinder-clone-v1-7697e.firebaseapp.com",
+    databaseURL: "https://tinder-clone-v1-7697e.firebaseio.com",
+    projectId: "tinder-clone-v1-7697e",
+    storageBucket: "tinder-clone-v1-7697e.appspot.com",
+    messagingSenderId: "217129175643",
+    appId: "1:217129175643:web:1d02dfa062524f72d88d7e",
+    measurementId: "G-QDR3JRNT0M"
+  };
+
+  
